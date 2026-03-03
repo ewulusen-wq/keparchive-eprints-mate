ea5db30767e5fb54d3153330d03c47f73c681d7c
Your Name <you@example.com>
2026-03-03 09:57:39 +0000

    Javítva: eprint_search_staff.pl konfigurációs hiba

--- cfg/cfg.d/eprint_search_staff.pl
+++ cfg/cfg.d/eprint_search_staff.pl
@@ -12,7 +12,7 @@ $c->{datasets}->{eprint}->{search}->{staff} =
 		{ meta_fields => [qw( userid.username )] },
 		{ meta_fields => [qw( userid.name )] },
 		{ meta_fields => [qw( dir )] },
-		@{$c->{search}{advanced}{search_fields}},
+		@{$c->{search}->{eprint}->{_default}->{fields}},
 	],
 	preamble_phrase => "Plugin/Screen/Staff/EPrintSearch:description",
 	title_phrase => "Plugin/Screen/Staff/EPrintSearch:title",
@@ -25,34 +25,3 @@ $c->{datasets}->{eprint}->{search}->{staff} =
 	show_zero_results => 1,
 	staff => 1,
 };
-
-=head1 COPYRIGHT
-
-=for COPYRIGHT BEGIN
-
-Copyright 2021 University of Southampton.
-EPrints 3.4 is supplied by EPrints Services.
-
-http://www.eprints.org/eprints-3.4/
-
-=for COPYRIGHT END
-
-=for LICENSE BEGIN
-
-This file is part of EPrints 3.4 L<http://www.eprints.org/>.
-
-EPrints 3.4 and this file are released under the terms of the
-GNU Lesser General Public License version 3 as published by
-the Free Software Foundation unless otherwise stated.
-
-EPrints 3.4 is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-See the GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with EPrints 3.4.
-If not, see L<http://www.gnu.org/licenses/>.
-
-=for LICENSE END
-
