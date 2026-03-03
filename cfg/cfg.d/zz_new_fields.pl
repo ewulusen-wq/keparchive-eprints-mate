ec4423734b463d1ac337a2d73747692c02279c43
Your Name <you@example.com>
2026-03-03 09:37:25 +0000

    Hozzáadva: facetták, böngészés menü és új mezők

---
+++ cfg/cfg.d/zz_new_fields.pl
@@ -0,0 +1,15 @@
+$c->add_dataset_field('eprint', {
+    name => 'photographers',
+    type => 'name',
+    multiple => 1,
+});
+
+$c->add_dataset_field('eprint', {
+    name => 'collection',
+    type => 'text',
+});
+
+$c->add_dataset_field('eprint', {
+    name => 'event_location',
+    type => 'text',
+});
